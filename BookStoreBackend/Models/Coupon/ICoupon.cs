8e549ac10de885a7d68f639f9dce3e567d2da930
adithyas330 <adithyas330@gmail.com>
2021-10-19 21:05:20 +0530

    controlers and Models of book +coupon

---
+++ BookStoreBackend/Models/Coupon/ICoupon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreBackend.Models
+{
+    interface ICoupon
+    {
+        List<Coupon> GetAllCoupon();
+        int AddCoupon(Coupon coupon);
+        int DeleteCoupon(int Id);
+        int UpdateCoupon(int Id,Coupon coupon);
+    }
+}
