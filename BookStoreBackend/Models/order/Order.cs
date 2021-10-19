b6499d77534867739a91ef45898ba8c8729fe7d0
AmaldevAD <amaldev.ad71868@gmail.com>
2021-10-19 15:54:27 +0530

    get order

---
+++ BookStoreBackend/Models/order/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreBackend.Models
+{
+    public class Order
+    {
+        public int orderId { get; set; }
+        public int UserId { get; set; }
+        public int bookId { get; set; }
+        public string orderPlacedAt  {get; set; }
+}
+}
